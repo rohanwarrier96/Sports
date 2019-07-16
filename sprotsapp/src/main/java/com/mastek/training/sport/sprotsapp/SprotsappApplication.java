4b0af7f05b4742f96594c20a55303b36e8f2b2cc
Rohan61175 <Rohan61175@GB-LD1LPW10G041.mastek.com>
2019-07-16 16:04:50 +0100

    test

---
+++ sprotsapp/src/main/java/com/mastek/training/sport/sprotsapp/SprotsappApplication.java
@@ -0,0 +1,13 @@
+package com.mastek.training.sport.sprotsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprotsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprotsappApplication.class, args);
+	}
+
+}
---
+++ sprotsapp/src/test/java/com/mastek/training/sport/sprotsapp/SprotsappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mastek.training.sport.sprotsapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SprotsappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
